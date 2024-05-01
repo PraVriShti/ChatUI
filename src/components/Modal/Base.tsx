b92a42cc76535f468b8f41cac5b680c85f960609
prtkjakhar <prtkjakhar@gmail.com>
2024-05-01 19:15:17 +0530

    style: popup ui enhancements

--- src/components/Modal/Base.tsx
+++ src/components/Modal/Base.tsx
@@ -43,7 +43,7 @@ export const Base = React.forwardRef<BaseModalHandle, ModalProps>((props, ref) =
     baseClass,
     active,
     className,
-    // title,
+    title,
     showClose = true,
     autoFocus = true,
     backdrop = true,
@@ -118,8 +118,8 @@ export const Base = React.forwardRef<BaseModalHandle, ModalProps>((props, ref) =
         <div className={`${baseClass}-content`}>
           <div className={`${baseClass}-header`}>
             <h5 className={`${baseClass}-title`} id={titleId}>
-              {/* {title} */}
-              <div style={{height: '2px', width: '55px', backgroundColor: '#B0B0B0', margin: 'auto'}}>
+              {title}
+              <div style={{height: '2px', width: '55px', backgroundColor: '#B0B0B0', margin: '10px auto 2px auto'}}>
               </div>
             </h5>
             {showClose && onClose && (
